602d53dfb76aa3411b5d1fa8824fe9f1f9e3a2b8
kwlee <kwlee@etri.re.kr>
2018-08-26 23:15:26 +0900

    fix ObjectsrequireNonNull problems.

--- src/main/java/camus/statechart/StatechartEventQueue.java
+++ src/main/java/camus/statechart/StatechartEventQueue.java
@@ -1,9 +1,9 @@
 package camus.statechart;
 
+import java.util.Objects;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.google.common.base.Preconditions;
 
 import event.Event;
 import event.EventSubscriber;
